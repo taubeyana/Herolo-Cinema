cf17c0b29cdbaaff715be5f94193ced7dfc723f3
Yana Taube <taubeyana@gmail.com>
2018-12-19 10:19:37 +0200

    header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import logo from '../../img/cinema.png';
 const Header = () => (
     <header className = "app-header">
         <img className = "logo" src = { logo } alt = "logo"/>
+        <h1> Movies-Library </h1>
     </header>
 )
 export default Header;

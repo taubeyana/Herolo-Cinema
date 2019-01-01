da32f7b90bb03f78647e49c6e3e8d4c72260485c
yana148 <yana148@gmail.com>
2019-01-01 11:44:41 +0200

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,10 @@
 import React  from 'react';
 import './Header.css';
-import logo from '../../img/cinema.png';
+import logo from '../../img/cinema-herolo.png';
 
 const Header = () => (
     <header className = "app-header">
         <img className = "logo" src = { logo } alt = "logo"/>
-        <h1> Movies-Library </h1>
     </header>
 )
 export default Header;
